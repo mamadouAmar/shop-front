28b112fc87a4c284eaee066848212bc2525247e6
quantumAmar <dmamadouamar@ept.sn>
2022-08-04 15:17:59 +0200

    Refactoring du routing des différents pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './components/accueil/accueil.component';
 import { AchatPageComponent } from './components/achat/achat-page/achat-page.component';
 import { AjoutAchatComponent } from './components/achat/ajout-achat/ajout-achat.component';
+import { ListAchatComponent } from './components/achat/list-achat/list-achat.component';
 import { ViewAchatComponent } from './components/achat/view-achat/view-achat.component';
 import { InventairePageComponent } from './components/inventaire/inventaire-page/inventaire-page.component';
 import { AjoutProduitComponent } from './components/produit/ajout-produit/ajout-produit.component';
+import { ListProduitComponent } from './components/produit/list-produit/list-produit.component';
 import { ProduitPageComponent } from './components/produit/produit-page/produit-page.component';
 import { ViewProduitComponent } from './components/produit/view-produit/view-produit.component';
 import { AjoutVenteComponent } from './components/vente/ajout-vente/ajout-vente.component';
+import { ListVenteComponent } from './components/vente/list-vente/list-vente.component';
 import { VentePageComponent } from './components/vente/vente-page/vente-page.component';
 import { ViewVenteComponent } from './components/vente/view-vente/view-vente.component';
 
@@ -16,18 +19,22 @@ const routes: Routes = [
   {path:'', component:AccueilComponent},
   {path:'produit', component:ProduitPageComponent,
   children : [
+    {path:'', component: ListProduitComponent},
+    {path: "edit", component: AjoutProduitComponent},
     {path:":id", component : ViewProduitComponent},
-    {path: "edit", component: AjoutProduitComponent}
   ]},
   {path:'vente', component:VentePageComponent,
   children : [
+    {path:'', component: ListVenteComponent},
+    {path: "edit", component: AjoutVenteComponent},
     {path:":id", component : ViewVenteComponent},
-    {path: "edit", component: AjoutVenteComponent}
   ]},
   {path:'achat', component:AchatPageComponent,
   children : [
+    {path:'', component: ListAchatComponent},
+    {path: "edit", component: AjoutAchatComponent},
     {path:":id", component : ViewAchatComponent},
-    {path: "edit", component: AjoutAchatComponent}
+
   ]},
   {path:'inventaire', component:InventairePageComponent},
 ];
