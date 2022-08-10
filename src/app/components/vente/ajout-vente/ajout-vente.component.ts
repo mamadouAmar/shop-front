22d056c0dd2011f24fbf22ae47b5464292ffad66
quantumAmar <dmamadouamar@ept.sn>
2022-08-10 21:37:04 +0200

    Implementation de l'auto complete pour l'ajout de vente

--- src/app/components/vente/ajout-vente/ajout-vente.component.ts
+++ src/app/components/vente/ajout-vente/ajout-vente.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
 import {Observable} from 'rxjs';
-import {map, startWith, switchMap} from 'rxjs/operators';
+import {distinctUntilChanged, map, startWith, switchMap, debounceTime} from 'rxjs/operators';
 import { LigneVenteForAdd } from 'src/app/models/ligne-vente';
-import { ProduitDTO, ReducedProduit } from 'src/app/models/produit';
+import { ProduitDTO, ProduitForAdd, ReducedProduit } from 'src/app/models/produit';
 import { VenteForAdd } from 'src/app/models/vente';
 import { ProduitService } from 'src/app/services/produit.service';
 import { VenteService } from 'src/app/services/vente.service';
@@ -17,30 +18,35 @@ export class AjoutVenteComponent implements OnInit {
 
   nouveauVente! : VenteForAdd;
 
+  @Input() datasource = new MatTableDataSource<LigneVenteForAdd>();
+
   venteFormGroup! : FormGroup;
 
   ligneVenteFormGroup! : FormGroup;
 
-  ligneVentes : Array<LigneVenteForAdd> = [];
+  ligneVentes : LigneVenteForAdd[] = [];
 
   produits! : ProduitDTO[];
+  loading!: boolean;
 
   constructor(private venteService : VenteService, 
     private produitService : ProduitService,
     private fbVente : FormBuilder,
     private fbLignevente : FormBuilder)
   {
-    this.getProduits();
+    
   }
 
-  myControl = new FormControl('');
-  options!: String[];
-  filteredOptions: Observable<String[]> | undefined;
+  selectedProduitControl = new FormControl('');
+  filteredOptions! : Observable<ProduitDTO[]>
+  options: ProduitDTO[] = [];
+  
 
   ngOnInit() {
+
     this.ligneVenteFormGroup = this.fbLignevente.group(
       {
-        produit : this.fbLignevente.control(String, [Validators.required]),
+        produit : this.fbLignevente.control(String(''), [Validators.required]),
         quantite : this.fbLignevente.control(Number, [Validators.required])
       }
     )
@@ -51,51 +57,68 @@ export class AjoutVenteComponent implements OnInit {
       }
     );
 
-    this.filteredOptions = this.myControl.valueChanges.pipe(
+    this.filteredOptions = this.selectedProduitControl.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value || '')),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(
+        (value) => 
+        {
+          return this._filter(value || ''); 
+        }
+        ),
     );
   }
 
-  private _filter(value: string): String[] {
-    const filterValue = value.toLowerCase();
-    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  private _filter(value: string): Observable<ProduitDTO[]> {
+    return this.produitService.getProduits()
+      .pipe(
+        map(response => response.filter(option => {
+            return option.libelle.toLowerCase().includes(value.toLowerCase())
+          }
+         )
+        )
+      )
   }
 
   displayedColumns: string[] = ['idProduit', 'libelle', 'quantite', 'total'];
 
+  getProduitByLibelle(libelle : string) : any{
+    for (const produit of this.produitService.produits) {
+      if(produit.libelle == libelle)
+        return produit;
+    }
+    return null
+  }
 
   public ajouterLigneVente() {
     if(this.ligneVenteFormGroup.valid){
       let lib = this.ligneVenteFormGroup.controls['produit'].value;
-      let index = this.options.indexOf(lib);
       let q = this.ligneVenteFormGroup.controls['quantite'].value;
-      let p = this.produits[index]
+      let p = this.getProduitByLibelle(lib)
       let pToAdd = new ReducedProduit(p.idProduit, p.libelle, p.coutUnitaire, p.prixVente)
       let lv = new LigneVenteForAdd(pToAdd, q);
       this.ligneVentes.push(lv);
+      this.datasource.data.push(lv);
     }
-    // this.ligneVentes.push(new LigneAchatForAdd(
-    //   l['produit'], l['quantite']
-    // ));
   }
 
-  getProduits(){
-    this.produitService.getProduits().subscribe(
-      (produits) => {
-        this.produits = produits;
-        this.getLibelleProduits();
-      }
-    );
-  }
+  // getProduits(){
+  //   this.produitService.getProduits().subscribe(
+  //     (produits) => {
+  //       this.produits = produits;
+  //       this.getLibelleProduits();
+  //     }
+  //   );
+  // }
 
-  public getLibelleProduits() : void{
-    let libelles: String[] = [];
-    for (let index = 0; index < this.produits.length; index++) {
-      libelles.push(this.produits[index].libelle);
-    }
-    this.options = libelles;
-  }
+  // public getLibelleProduits() : void{
+  //   let libelles: String[] = [];
+  //   for (let index = 0; index < this.produits.length; index++) {
+  //     libelles.push(this.produits[index].libelle);
+  //   }
+  //   this.options = libelles;
+  // }
 
   public enregistrerVente(){
     this.nouveauVente = new VenteForAdd(
--- src/app/services/produit.service.ts
+++ src/app/services/produit.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Produit, ProduitDTO, ProduitForAdd } from '../models/produit';
 
@@ -34,19 +34,30 @@ export class ProduitService {
     );
   }
 
+  produits : ProduitDTO[] = []
+
   public getProduits() : Observable<ProduitDTO[]>{
+
     return this.http.get<ProduitDTO[]>(
       `${this.apiServerUrl}/dtos`
-    );
+    ).pipe(
+      tap(
+        data => this.produits = data
+      )
+    )
+
+    // return this.http.get<ProduitDTO[]>(
+    //   `${this.apiServerUrl}/dtos`
+    // );
   }
 
-  public get(id:Number) : Observable<any>{
+  public get(id:Number) : Observable<Produit>{
     return this.http.get<any>(
       `${this.apiServerUrl}/${id}`
     );
   }
 
-  public post(produit : ProduitForAdd) : Observable<any>{
+  public post(produit : ProduitForAdd) : Observable<Produit>{
     return this.http.post<any>(
       `${this.apiServerUrl}`, produit
     );
